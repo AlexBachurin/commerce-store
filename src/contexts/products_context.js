16b4085ed9ccea8cdb4d96aa972b12057087b541
Alex <alexbachurin91@gmail.com>
2022-02-01 09:24:21 +0300

    fetch products and featured products completed

--- src/contexts/products_context.js
+++ src/contexts/products_context.js
@@ -1,10 +1,16 @@
 import React, { useContext, useEffect, useReducer } from "react";
 import products_reducer from "../reducers/products_reducer";
-import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+import { SIDEBAR_OPEN, SIDEBAR_CLOSE, GET_PRODUCTS_BEGIN, GET_PRODUCTS_ERROR, GET_PRODUCTS_SUCCESS } from "../actions";
+import { products_url } from "../utils.js/constants";
+import axios from 'axios'
 const ProductsContext = React.createContext();
 
 const iniitialState = {
     isSidebarOpen: false,
+    featuredProducts: [],
+    products: [],
+    products_loading: false,
+    products_error: false
 
 };
 
@@ -18,6 +24,27 @@ export const ProductsProvider = ({ children }) => {
     const closeSidebar = () => {
         dispatch({ type: SIDEBAR_CLOSE })
     }
+
+    /// *** FETCH PRODUCTS ***
+    const fetchProducts = async () => {
+        //first dispatch that we beginning to fetch , so we can turn loading to true
+        dispatch({ type: GET_PRODUCTS_BEGIN })
+        try {
+            const response = await axios.get(`${products_url}`)
+            console.log(response);
+            //if we getting response, display data and set products and featured products
+            if (response) {
+                dispatch({ type: GET_PRODUCTS_SUCCESS, payload: response.data })
+            }
+        } catch (error) {
+            //if some error while fetching occured dispatch an error
+            dispatch({ type: GET_PRODUCTS_ERROR })
+        }
+    }
+
+    useEffect(() => {
+        fetchProducts()
+    }, [])
     return <ProductsContext.Provider value={{
         ...state,
         openSidebar,
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -1,10 +1,25 @@
-import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+import { SIDEBAR_OPEN, SIDEBAR_CLOSE, GET_PRODUCTS_BEGIN, GET_PRODUCTS_SUCCESS, GET_PRODUCTS_ERROR } from "../actions";
+
+
 const products_reducer = (state, action) => {
     if (action.type === SIDEBAR_OPEN) {
         return { ...state, isSidebarOpen: true }
     }
     if (action.type === SIDEBAR_CLOSE) {
         return { ...state, isSidebarOpen: false }
+    }
+    if (action.type === GET_PRODUCTS_BEGIN) {
+        return { ...state, products_loading: true }
+    }
+    if (action.type === GET_PRODUCTS_SUCCESS) {
+        //get featured products aswell by filtering data we get in payload, also slice it to get only 3 products, since we dont need them all
+        const featuredProducts = action.payload.filter(item => item.featured === true).slice(0, 3)
+        return { ...state, products: action.payload, featuredProducts, products_loading: false }
+    }
+    if (action.type === GET_PRODUCTS_ERROR) {
+        console.log('error occured')
+        //if error occured disable loading and set error state to true
+        return { ...state, products_error: true, products_loading: false }
     }
     throw new Error('No matching action type')
 }
