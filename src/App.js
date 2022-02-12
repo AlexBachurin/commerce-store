2c30d8f1c5cc1545ef8fe27716eecbca03972e0e
Alex <alexbachurin91@gmail.com>
2022-02-12 22:24:00 +0300

    redirect to home page if user is not authenticated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Footer, Navbar, Sidebar } from "./components";
 import { HomePage, AboutPage, ProductsPage, SingleProductPage, CartPage, ErrorPage, CheckoutPage } from './pages';
 function App() {
+  const { isAuthenticated, user } = useAuth0();
+  const isUser = isAuthenticated && user;
   return (
     <Router>
       <Navbar />
@@ -13,7 +16,8 @@ function App() {
         <Route path='/products' element={<ProductsPage />} />
         <Route path='/products/:id' element={<SingleProductPage />} />
         <Route path='/cart' element={<CartPage />} />
-        <Route path='/checkout' element={<CheckoutPage />} />
+        {/* //if user authenticated we can go to checkout page, if not then redirect to home page */}
+        <Route path='/checkout' element={isUser ? <CheckoutPage /> : <Navigate to={'/'} />} />
         <Route path='*' element={<ErrorPage />} />
       </Routes>
       <Footer />
