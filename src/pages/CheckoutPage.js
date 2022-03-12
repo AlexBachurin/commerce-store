62f66e8b2719dce853f5a4696de307c5152e8bea
Alex <alexbachurin91@gmail.com>
2022-03-12 12:01:19 +0300

    added checkout to navigation

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,7 +1,16 @@
 import React from 'react';
-
+import styled from 'styled-components'
+import { HeroNavigation, StripeCheckout } from '../components';
 const CheckoutPage = () => {
-    return <div> CheckoutPage</div>;
+    return <main>
+        <HeroNavigation pageName='Checkout' />
+        <Wrapper className='page'>
+            <h1>checkout here</h1>
+        </Wrapper>
+    </main>;
 };
 
+const Wrapper = styled.div`
+
+`
 export default CheckoutPage;
