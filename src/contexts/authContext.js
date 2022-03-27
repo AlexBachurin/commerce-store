bdc597934d271f111c2c5fed6b43db792ef79627
Alex <alexbachurin91@gmail.com>
2022-03-27 16:06:16 +0300

    authContext functionality

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,11 +1,43 @@
 import React, { useContext, useState } from "react";
-
+import { auth } from '../firebase';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = React.createContext();
 
 const AuthProvider = ({ children }) => {
-    return <AuthContext.Provider value={{
+    //state for user 
+    const [user, setUser] = useState(null);
+    //state for checking if user is Authenticated
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    let navigate = useNavigate();
+    ///Sing in functionality
+    const signIn = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // The signed-in user info.
+                const user = result.user;
+                if (user) {
+                    setIsLoggedIn(true);
+                    setUser(user);
+                    navigate('/')
+                }
+            }).catch((error) => {
+                console.log(error);
+            });
+    }
 
+    //logout
+    const logout = () => {
+        setIsLoggedIn(false)
+        auth.signOut();
+    }
+    return <AuthContext.Provider value={{
+        user,
+        isLoggedIn,
+        signIn,
+        logout
     }}>
         {children}
     </AuthContext.Provider>
